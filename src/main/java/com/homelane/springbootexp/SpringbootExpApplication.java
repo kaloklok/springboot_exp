837fb83ac8d4bd723fdd811bfd05989ce9f19bdd
kaloklok <kaloklok@gmail.com>
2017-06-04 15:12:39 +0800

    initial version

---
+++ src/main/java/com/homelane/springbootexp/SpringbootExpApplication.java
@@ -0,0 +1,12 @@
+package com.homelane.springbootexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExpApplication.class, args);
+	}
+}
